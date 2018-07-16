10a34a5c4b089fd5b5088e05bb88dc026e816c1f
Asbefore1 <@3497639925qq.com>
2018-07-16 18:21:15 +0800

    h

---
+++ mcart/js/rem.js
@@ -0,0 +1,14 @@
+(function(win,doc){
+	//1.获取根元素
+	var oRoot=doc.getElementsByTagName('html')[0];
+	function refreshFontSize(){			
+		//2.获取设备的宽
+		var iWidth=doc.body.clientWidth ||doc.documentElement.clientWidth;
+		console.log(iWidth);
+		//3.根据设备的宽计算出根元素的fontSize
+		var iFontSize=iWidth/10;
+		oRoot.style.fontSize=iFontSize+'px';
+	}
+	win.addEventListener('resize',refreshFontSize,false);
+	doc.addEventListener('DOMContentLoaded',refreshFontSize,false);
+})(window,document);
